bff0578928898840bac7a06b8f57032efa5a013d
mthdht <mthdht@gmail.com>
2024-11-29 10:14:35 +0100

    add last activities section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@
         </nav>
     </header>
     
-    <main class="p-4 bg-red-50">
+    <main class="p-4 space-y-8">
         <section class="rounded-lg overflow-hidden bg-yellow-200 relative">
             <img src="" alt="" class="w-full object-cover h-96 max-h-96">
             <p class="absolute bottom-4 px-4 flex w-full gap-4 items-center">
@@ -16,5 +16,56 @@
                 <span class="font-bold text-sm">7.5 kg</span>
             </p>
         </section>
+
+        <section class="font-semibold">
+            <h3 class="font-bold mb-4 text-xl">Dernière activités</h3>
+            <ul class="space-y-2">
+                <li class="flex gap-6 bg-sky-50 px-3 py-2 items-center rounded shadow">
+                    <div class="icon">
+                       <FoodIcon class="size-6 fill-sky-500"></FoodIcon>
+                    </div>
+                    <div class="label text-lg grow">Repas</div>
+                    <div class="value">210 ml</div>
+                    <div class="time">14h30</div>
+                </li>
+
+                <li class="flex gap-6 bg-sky-50 px-3 py-2 items-center rounded shadow">
+                    <div class="icon">
+                       <FoodIcon class="size-6 fill-sky-500"></FoodIcon>
+                    </div>
+                    <div class="label text-lg grow">Repas</div>
+                    <div class="value">210 ml</div>
+                    <div class="time">14h30</div>
+                </li>
+
+                <li class="flex gap-6 bg-sky-50 px-3 py-2 items-center rounded shadow">
+                    <div class="icon">
+                       <FoodIcon class="size-6 fill-sky-500"></FoodIcon>
+                    </div>
+                    <div class="label text-lg grow">Repas</div>
+                    <div class="value">210 ml</div>
+                    <div class="time">14h30</div>
+                </li>
+
+                <li class="flex gap-6 bg-sky-50 px-3 py-2 items-center rounded shadow">
+                    <div class="icon">
+                       <FoodIcon class="size-6 fill-sky-500"></FoodIcon>
+                    </div>
+                    <div class="label text-lg grow">Repas</div>
+                    <div class="value">210 ml</div>
+                    <div class="time">14h30</div>
+                </li>
+
+                <li class="flex gap-6 bg-sky-50 px-3 py-2 items-center justify-center rounded shadow">
+                    <button class="font-bold">Voir plus +</button>
+                </li>
+            </ul>
+        </section>
     </main>
 </template>
+
+<script setup>
+import FoodIcon from './FoodIcon.vue'
+import Test from './Test.vue'
+
+</script>
