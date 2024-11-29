a2e40e02f908c324c1df8f00846442c0e8f9edea
mthdht <mthdht@gmail.com>
2024-11-29 09:25:39 +0100

    made baby image section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,20 @@
 <template>
+    <header class="relative flex justify-center items-center bg-sky-500 p-4 text-white">
+        <div class="brand font-semibold text-xl">My Baby</div>
+        <nav class="flex gap-4 absolute right-8">
+            <a href="">ok</a>
+            <a href="">ok</a>
+        </nav>
+    </header>
     
+    <main class="p-4 bg-red-50">
+        <section class="rounded-lg overflow-hidden bg-yellow-200 relative">
+            <img src="" alt="" class="w-full object-cover h-96 max-h-96">
+            <p class="absolute bottom-4 px-4 flex w-full gap-4 items-center">
+                <span class="grow font-bold text-xl">Oscar</span>
+                <span class="font-bold text-sm">6mois</span>
+                <span class="font-bold text-sm">7.5 kg</span>
+            </p>
+        </section>
+    </main>
 </template>
