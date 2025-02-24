5e875c6d6e85a078a2608d6b29b8c7a21fca33c6
mthdht <mthdht@gmail.com>
2025-02-24 14:34:01 +0100

    add stats section to home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,14 @@
 <template>
-   
+   <section class="stats p-4 flex gap-4 font-semibold">
+        <div class="w-1/2 border h-24 rounded shadow flex justify-center items-center gap-2">
+           <p>3</p>
+           <p>crèches</p>
+        </div>
+        <div class="w-1/2 border h-24 rounded shadow flex flex-col justify-center items-center">
+            <p>32 enfants</p>
+            <p class="text-xs font-normal">27 présents</p>
+        </div>
+   </section>
 </template>
 
 <script setup>
