8fde6ba4b9cd0c692d32e64c5ac8a8a015c17824
mthdht <mthdht@gmail.com>
2025-02-25 14:39:31 +0100

    made child card

--- src/pages/Children.vue
+++ src/pages/Children.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="p-4 space-y-6">
+        <h2 class="font-semibold text-xl flex gap-3">
+            <UserIcon class="size-7 text-indigo-500"></UserIcon>
+            Gerer les enfants !
+        </h2>
+
         <section class="filters flex gap-4">
             <Select v-model="filters.creche" :options="['creche 1', 'creche 2']" class="grow"></Select>
             <Select v-model="filters.sort" :options="['Nom', 'Prénom']" class="grow"></Select>
@@ -9,8 +14,10 @@
         </section>
 
         <section class="children grid grid-cols-3 gap-4">
-            <div class="child aspect-square border shadow" v-for="(child, index) in filteredChildren">
-                {{ child.name }}
+            <div class="child aspect-square border shadow flex flex-col justify-between items-center p-2 relative rounded" v-for="(child, index) in filteredChildren">
+                <div class="avatar self-center size-16 rounded-full border"></div>
+                
+                <span class="grow font-semibold">{{  child.name }}</span>
             </div>
         </section>
     </div>
@@ -19,7 +26,7 @@
 <script setup>
 import { computed, reactive } from 'vue'
 import Select from '../components/Select.vue'
-import { UserPlusIcon } from '@heroicons/vue/24/outline';
+import { UserPlusIcon, UserIcon } from '@heroicons/vue/24/outline';
 
 const filters = reactive({
     creche: "creche 1",
@@ -29,33 +36,41 @@ const filters = reactive({
 const children = [
     {
         name: "Henry",
-        creche: "creche 1"
+        creche: "creche 1",
+        age: "6 mois"
     },
     {
         name: "Stephanie",
-        creche: "creche 1"
+        creche: "creche 1",
+        age: "6 mois"
     },
     {
         name: "Stephanie",
-        creche: "creche 2"
+        creche: "creche 2",
+        age: "6 mois"
     },
     {
         name: "Stephanie",
-        creche: "creche 2"
+        creche: "creche 2",
+        age: "6 mois"
     },
     {
         name: "Oscar",
-        creche: "creche 1"
+        creche: "creche 1",
+        age: "6 mois"
     },
     {
         name: "Arthur",
-        creche: "creche 2"
+        creche: "creche 2",
+        age: "6 mois"
     },
     {
-        name: "Stephanie"
+        name: "Stephanie",
+        age: "6 mois"
     },
     {
-        name: "Stephanie"
+        name: "Stephanie",
+        age: "6 mois"
     },
     {
         name: "Stephanie"
