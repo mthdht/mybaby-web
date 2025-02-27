1ac244bbc1b981ad8232a4451f06f7e33aa6e29d
mthdht <mthdht@gmail.com>
2025-02-27 10:48:16 +0100

    create acordion component for child details

---
+++ src/components/Accordion.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="accordion space-y-4">
+        <p class="font-semibold flex justify-between items-center cursor-pointer" @click="showContent = !showContent">
+            <span>{{ props.label }}</span>
+            <span><ChevronDownIcon class="size-6 stroke-2"></ChevronDownIcon></span>
+        </p>
+        <Transition
+            enter-active-class="transition duration-200 ease-in-out"
+            leave-active-class="transition duration-200 ease-in-out"
+            enter-from-class="opacity-0 -translate-y-2"
+            leave-to-class="opacity-0 -translate-y-2"
+        >
+            <div class="accordion-content"v-show="showContent">
+                <slot></slot>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { ChevronDownIcon } from '@heroicons/vue/24/outline';
+
+const props = defineProps({
+    label: String,
+    open: Boolean
+})
+
+const showContent = ref(props.open)
+</script>
--- src/pages/Child.vue
+++ src/pages/Child.vue
@@ -1,56 +1,57 @@
 <template>
-    <div class="p-4 space-y-6">
-      <!-- En-tête avec les informations de base -->
-      <header class="flex gap-4 items-center">
-        <div class="avatar w-24 h-24 rounded-full overflow-hidden border-2" :class="[child.sexe == 'male' ? 'border-sky-500' : 'border-pink-500']">
-          <img :src="child.avatar || 'default-avatar.png'" alt="avatar de l'enfant" class="object-cover w-full h-full">
-        </div>
-        <div class="info">
-          <h3 class="font-semibold text-2xl">{{ child.name }}</h3>
-          <p class="text-slate-500">{{ child.age }} | <span :class="{ 'text-sky-500': child.sexe === 'male', 'text-pink-500': child.sexe === 'female' }">{{ child.sexe == 'male' ? 'Garçon': 'Fille' }}</span></p>
-        </div>
-      </header>
-  
-      <!-- Tabs ou sections de contenu -->
-      <section class="tabs flex gap-4 font-semibold">
-        <button class="tab-btn border-2 border-sky-500 text-sky-500 py-2 px-4 rounded w-1/3 " 
-          :class="{ 'bg-sky-500 text-white': activeTab === 'activities' }" @click="activeTab = 'activities'"
-        >
-          Activités
-        </button>
+  <div class="p-4 space-y-6">
+    <!-- En-tête avec les informations de base -->
+    <header class="flex gap-4 items-center">
+      <div class="avatar w-24 h-24 rounded-full overflow-hidden border-2" :class="[child.sexe == 'male' ? 'border-sky-500' : 'border-pink-500']">
+        <img :src="child.avatar || 'default-avatar.png'" alt="avatar de l'enfant" class="object-cover w-full h-full">
+      </div>
+      <div class="info">
+        <h3 class="font-semibold text-2xl">{{ child.name }}</h3>
+        <p class="text-slate-500">{{ child.age }} | <span :class="{ 'text-sky-500': child.sexe === 'male', 'text-pink-500': child.sexe === 'female' }">{{ child.sexe == 'male' ? 'Garçon': 'Fille' }}</span></p>
+      </div>
+    </header>
 
-        <button class="tab-btn border-2 border-emerald-500 text-emerald-500 py-2 px-4 rounded w-1/3 " 
-          :class="{ 'bg-emerald-500 text-white': activeTab === 'journal' }" @click="activeTab = 'journal'"
-        >
-          Journal
-        </button>
-        <button class="tab-btn border-2 border-yellow-500 py-2 px-4 rounded w-1/3 " 
-          :class="{ 'bg-yellow-500 text-white': activeTab === 'alerts', 'text-yellow-500': activeTab !== 'alerts' }" @click="activeTab = 'alerts'"
-        >
-          Alertes
-        </button>
-      </section>
-  
-      <!-- Contenu de l'onglet sélectionné -->
-      <Transition mode="out-in"
-          enter-active-class="transition duration-300 ease-out"
-          leave-active-class="transition duration-300 ease-in"
-          enter-from-class="opacity-0 -translate-y-1"
-          leave-to-class="opacity-0 translate-y-1"
+    <!-- Tabs ou sections de contenu -->
+    <section class="tabs flex gap-4 font-semibold">
+      <button class="tab-btn border-2 border-sky-500 text-sky-500 py-2 px-4 rounded w-1/3 " 
+        :class="{ 'bg-sky-500 text-white': activeTab === 'journal' }" @click="activeTab = 'journal'"
       >
+        Journaux 
+      </button>
 
-        <div v-if="activeTab === 'activities'">
-          <!-- Liste des activités -->
-          <div class="activity-card bg-sky-100 p-4 rounded shadow">
-            <h4 class="font-semibold">Activités de la journée</h4>
+      <button class="tab-btn border-2 border-emerald-500 text-emerald-500 py-2 px-4 rounded w-1/3 " 
+        :class="{ 'bg-emerald-500 text-white': activeTab === 'messages' }" @click="activeTab = 'messages'"
+      >
+        Messages
+      </button>
+      <button class="tab-btn border-2 border-yellow-500 py-2 px-4 rounded w-1/3 " 
+        :class="{ 'bg-yellow-500 text-white': activeTab === 'informations', 'text-yellow-500': activeTab !== 'informations' }" @click="activeTab = 'informations'"
+      >
+        Informations
+      </button>
+    </section>
+
+    <!-- Contenu de l'onglet sélectionné -->
+    <Transition mode="out-in"
+        enter-active-class="transition duration-200 ease-out"
+        leave-active-class="transition duration-200 ease-in"
+        enter-from-class="opacity-0 -translate-y-2"
+        leave-to-class="opacity-0 translate-y-2"
+    >
+
+      <div v-if="activeTab === 'journal'">
+        <!-- Liste des activités -->
+        <div class="activity-card bg-sky-100 p-4 rounded shadow">
+          <Accordion label="Lundi 24 Février 2025" open>
             <ul>
-            <li>Repas: <span>10h30 - Purée de légumes</span></li>
-            <li>Coucher: <span>12h - Sieste de 2 heures</span></li>
-          </ul>
+              <li>Repas: <span>10h30 - Purée de légumes</span></li>
+              <li>Coucher: <span>12h - Sieste de 2 heures</span></li>
+            </ul>
+          </Accordion>
         </div>
       </div>
       
-      <div v-else-if="activeTab === 'journal'">
+      <div v-else-if="activeTab === 'messages'">
         <!-- Journal quotidien -->
         <div class="journal-card bg-emerald-100 p-4 rounded shadow">
           <h4 class="font-semibold">Journal de la journée</h4>
@@ -58,7 +59,7 @@
         </div>
       </div>
       
-      <div v-else-if="activeTab === 'alerts'">
+      <div v-else-if="activeTab === 'informations'">
         <!-- Alertes et messages -->
         <div class="alert-card bg-yellow-50 p-4 rounded shadow">
           <p class="font-semibold text-red-600">Alerte</p>
@@ -66,23 +67,20 @@
         </div>
       </div>
     </Transition>
-    </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
-  import { ref } from 'vue'
-  
-  const child = {
-    name: "Henry Dubois",
-    age: "6 mois",
-    sexe: "male",
-    avatar: "https://placekitten.com/200/200"
-  }
-  
-  const activeTab = ref('activities')
-  </script>
-  
-  <style scoped>
+<script setup>
+import { ref } from 'vue'
+import Accordion from '../components/Accordion.vue';
 
-  </style>
+const child = {
+  name: "Henry Dubois",
+  age: "6 mois",
+  sexe: "male",
+  avatar: "https://placekitten.com/200/200"
+}
+
+const activeTab = ref('journal')
+</script>
   
